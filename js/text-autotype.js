e376c2c86f6ebf9af6cb0eb663ee0a055df602d5
Mohak Bajaj <bmohak87@gmail.com>
2024-02-27 20:48:56 +0530

    Update text-autotype.js

--- js/text-autotype.js
+++ js/text-autotype.js
@@ -1,6 +1,6 @@
 var i = 1;
 var j= -1;
-var list = ['Pythonista | Machine Learning Enthusiast | Full Stack Developer'];
+var list = ['DevSecOps Enthusiast | Full Stack Developer'];
 var a=list[0];
 var len = list.length;
 var num = 0;
